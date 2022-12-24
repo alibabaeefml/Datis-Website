4265b9453ef2a3037df128400ab836ab9f5f373f
Ali Babaee <alisource79@gmail.com>
2022-12-24 19:34:56 +0330

    comit

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -26,7 +26,7 @@ jQuery(function () {
     spaceBetween: 0,
     speed: 1500,
     width: 700,
-    navigation:{
+    navigation: {
       nextEl: "#card_swiper_right",
       prevEl: "#card_swiper_left",
     },
@@ -34,7 +34,7 @@ jQuery(function () {
       delay: 1500,
       disableOnInteraction: false,
     },
-    loop:true
+    loop: true,
   });
 
   // TypeWriter Function
@@ -94,176 +94,173 @@ jQuery(function () {
   `;
   typewrite(section_one_code, $("#section-one .typewriter").get(0));
   // Initialize fullpage
-  $("#fullpage-container").fullpage({
-    slidesNavigation: true,
-    scrollHorizontally: true,
-    afterLoad: function (section, origin) {
-      origin.item == $("#section-one").get(0)
-        ? $("#fullpage-container").css("transform", "unset")
-        : null;
-      if (section && section.item == $("#section-one").get(0)) {
-        $(".specialties-title, .specialties-list").css(
-          "opacity",
-          "1"
-        );
-        $(".specialties-list").removeClass("scale-0");
-        if (innerWidth > 1024) {
-          setTimeout(() => {
-            $(".specialties-list").width("75%");
-          }, 500);
-        }
-      }
-    },
-    afterSlideLoad() {
-      if ($("#slide-three.active").length) {
-        
-
-        // Section Three Typewriting css-code
-        let section_three_code = `.my-card {
-          height: max-content;
-        }
-        .avatars-div {
-          justify-content: space-between;
-        }
-        .rounded {
-          border-radius: 40px;
-        }
-        `;
-        const match_card_style = () => {
-          if (
-            $("#section-three .typewriter-dark")
-              .text()
-              .includes("height: max-content")
-          ) {
-            $("#slide-three-card").addClass("h-[440px]");
-          }
-          if (
-            $("#section-three .typewriter-dark")
-              .text()
-              .includes("space-between")
-          ) {
-            $("#avatars-ul").addClass("justify-between");
-          }
-          if (
-            $("#section-three .typewriter-dark")
-              .text()
-              .includes("border-radius: 40px")
-          ) {
-            $(".rounded-none").removeClass("rounded-none");
-          }
-        };
-        typewrite(
-          section_three_code,
-          $("#section-three .typewriter-dark").get(0),
-          match_card_style
-        );
-      }
-      if ($("#slide-four.active").length) {
-        const enter_password = () => {
-          // enter password
-          $("#sample_pass_inp").css("border-color", "red");
-          $("#datis_pass_inp").css("border-color", "rgb(72, 1, 255)");
-          typewrite(
-            `password`,
-            $("#sample_pass_inp").get(0),
-            null,
-            "input",
-            150
-          );
-          typewrite(
-            `password`,
-            $("#datis_pass_inp").get(0),
-            null,
-            "input",
-            150
-          );
-
-          setTimeout(() => {
-            $(".pass-input").css("border-color", "rgb(209, 213, 219)");
-            $("#sample_page_loader").toggleClass("scale-0");
-            counter(0, 100, 10, "#datis_load_time");
-            counter(0, 600, 10, "#sample_load_time");
+  if (!isMobile()) {
+    alert("triggered")
+    $("#fullpage-container").fullpage({
+      slidesNavigation: true,
+      scrollHorizontally: true,
+      afterLoad: function (section, origin) {
+        origin.item == $("#section-one").get(0)
+          ? $("#fullpage-container").css("transform", "unset")
+          : null;
+        if (section && section.item == $("#section-one").get(0)) {
+          $(".specialties-title, .specialties-list").css("opacity", "1");
+          $(".specialties-list").removeClass("scale-0");
+          if (innerWidth > 1024) {
             setTimeout(() => {
-              $("#datis_webpage").width("100%");
-            }, 800);
-            setTimeout(() => {
-              $("#sample_webpage").width("100%");
-            }, 6000);
-          }, 3500);
-        };
-        const enter_phone = () => {
-          // enter phone number
-          $("#sample_phone_inp").css("border-color", "red");
-          $("#datis_phone_inp").css("border-color", "rgb(72, 1, 255)");
-          typewrite(
-            `09120000000`,
-            $("#datis_phone_inp").get(0),
-            null,
-            "input",
-            150
-          );
-          typewrite(
-            `09650000000`,
-            $("#sample_phone_inp").get(0),
-            null,
-            "input",
-            150
-          );
-          setTimeout(() => {
-            $(".phone-input").css("border-color", "rgb(209, 213, 219)");
-            enter_password();
-          }, 3500);
-        };
-
-        const view_load_animation = () => {
-          if (!globalThis.slide_4_loaded) {
-            globalThis.slide_4_loaded = true;
-            $(".web-card").css({ margin: 0, opacity: 1 });
-            enter_phone();
+              $(".specialties-list").width("75%");
+            }, 500);
           }
-        };
-        view_load_animation();
-      }
-      if ($("#slide-five.active").length) {
-        typewrite(
-          "جستجوی مربوط به سایت شما",
-          $("#google_search_inp").get(0),
-          null,
-          "input",
-          0
-        );
-        const switch_google_results = async () => {
-          let difference = parseInt(
-            $("#gr_item_2").offset().top - $("#gr_item_1").offset().top
+        }
+      },
+      afterSlideLoad() {
+        if ($("#slide-three.active").length) {
+          // Section Three Typewriting css-code
+          let section_three_code = `.my-card {
+            height: max-content;
+          }
+          .avatars-div {
+            justify-content: space-between;
+          }
+          .rounded {
+            border-radius: 40px;
+          }
+          `;
+          const match_card_style = () => {
+            if (
+              $("#section-three .typewriter-dark")
+                .text()
+                .includes("height: max-content")
+            ) {
+              $("#slide-three-card").addClass("h-[440px]");
+            }
+            if (
+              $("#section-three .typewriter-dark")
+                .text()
+                .includes("space-between")
+            ) {
+              $("#avatars-ul").addClass("justify-between");
+            }
+            if (
+              $("#section-three .typewriter-dark")
+                .text()
+                .includes("border-radius: 40px")
+            ) {
+              $(".rounded-none").removeClass("rounded-none");
+            }
+          };
+          typewrite(
+            section_three_code,
+            $("#section-three .typewriter-dark").get(0),
+            match_card_style
           );
-          $("#gr_item_4").css("translate", `0px ${-difference}px`);
-          $("#gr_item_3").css("translate", `0px ${difference}px`);
-          setTimeout(() => {
-            $("#gr_item_4").css("translate", `0px ${-difference * 2}px`);
-            $("#gr_item_2").css("translate", `0px ${difference}px`);
-          }, 2000);
-          setTimeout(() => {
-            $("#gr_item_4").css("translate", `0px ${-difference * 3}px`);
-            $("#gr_item_1").css("translate", `0px ${difference}px`);
-          }, 4000);
-        };
+        }
+        if ($("#slide-four.active").length) {
+          const enter_password = () => {
+            // enter password
+            $("#sample_pass_inp").css("border-color", "red");
+            $("#datis_pass_inp").css("border-color", "rgb(72, 1, 255)");
+            typewrite(
+              `password`,
+              $("#sample_pass_inp").get(0),
+              null,
+              "input",
+              150
+            );
+            typewrite(
+              `password`,
+              $("#datis_pass_inp").get(0),
+              null,
+              "input",
+              150
+            );
 
-        const load_google_results_view = () => {
-          if (!globalThis.slide_5_loaded) {
-            globalThis.slide_5_loaded = true;
             setTimeout(() => {
-              $("#google_results_view").removeClass("hidden");
-              $("#seo_desc_div").animate({ left: "133px" }, 1000);
-              counter(0, 100, 70, "#seo_percentage");
+              $(".pass-input").css("border-color", "rgb(209, 213, 219)");
+              $("#sample_page_loader").toggleClass("scale-0");
+              counter(0, 100, 10, "#datis_load_time");
+              counter(0, 600, 10, "#sample_load_time");
+              setTimeout(() => {
+                $("#datis_webpage").width("100%");
+              }, 800);
+              setTimeout(() => {
+                $("#sample_webpage").width("100%");
+              }, 6000);
+            }, 3500);
+          };
+          const enter_phone = () => {
+            // enter phone number
+            $("#sample_phone_inp").css("border-color", "red");
+            $("#datis_phone_inp").css("border-color", "rgb(72, 1, 255)");
+            typewrite(
+              `09120000000`,
+              $("#datis_phone_inp").get(0),
+              null,
+              "input",
+              150
+            );
+            typewrite(
+              `09650000000`,
+              $("#sample_phone_inp").get(0),
+              null,
+              "input",
+              150
+            );
+            setTimeout(() => {
+              $(".phone-input").css("border-color", "rgb(209, 213, 219)");
+              enter_password();
+            }, 3500);
+          };
+
+          const view_load_animation = () => {
+            if (!globalThis.slide_4_loaded) {
+              globalThis.slide_4_loaded = true;
+              $(".web-card").css({ margin: 0, opacity: 1 });
+              enter_phone();
+            }
+          };
+          view_load_animation();
+        }
+        if ($("#slide-five.active").length) {
+          typewrite(
+            "جستجوی مربوط به سایت شما",
+            $("#google_search_inp").get(0),
+            null,
+            "input",
+            0
+          );
+          const switch_google_results = async () => {
+            let difference = parseInt(
+              $("#gr_item_2").offset().top - $("#gr_item_1").offset().top
+            );
+            $("#gr_item_4").css("translate", `0px ${-difference}px`);
+            $("#gr_item_3").css("translate", `0px ${difference}px`);
+            setTimeout(() => {
+              $("#gr_item_4").css("translate", `0px ${-difference * 2}px`);
+              $("#gr_item_2").css("translate", `0px ${difference}px`);
+            }, 2000);
+            setTimeout(() => {
+              $("#gr_item_4").css("translate", `0px ${-difference * 3}px`);
+              $("#gr_item_1").css("translate", `0px ${difference}px`);
             }, 4000);
-            setTimeout(() => {
-              switch_google_results();
-            }, 7000);
-          }
-        };
-        load_google_results_view();
-      }
-    },
-  });
-  isMobile() ? $("#fullpage-container").fullpage.destroy() : null;
+          };
+
+          const load_google_results_view = () => {
+            if (!globalThis.slide_5_loaded) {
+              globalThis.slide_5_loaded = true;
+              setTimeout(() => {
+                $("#google_results_view").removeClass("hidden");
+                $("#seo_desc_div").animate({ left: "133px" }, 1000);
+                counter(0, 100, 70, "#seo_percentage");
+              }, 4000);
+              setTimeout(() => {
+                switch_google_results();
+              }, 7000);
+            }
+          };
+          load_google_results_view();
+        }
+      },
+    });
+  }
 });
