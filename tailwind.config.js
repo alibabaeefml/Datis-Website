c8d8a66515cb72589caaaa1816527eb22481e8f7
alibabaeefml <alisource79@gmail.com>
2023-01-01 10:14:08 +0330

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public_html/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
       screens: {
